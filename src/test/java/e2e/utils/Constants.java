8e32341f387b3f664c4e79b711c52d6add511cc4
tmertt94 <93171908+tmertt94@users.noreply.github.com>
2023-09-02 15:40:38 -0400

    Update Constants.java

--- src/test/java/e2e/utils/Constants.java
+++ src/test/java/e2e/utils/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 
     public static final String Configuration_FilePath=System.getProperty("user.dir") + "/src/test/resources/config/Configuration.properties";
 
-
     public static final Integer implicitly_wait=10;
 
     public static final String ScreenShot_FilePath=System.getProperty("user.dir") + "/screenshots/";
